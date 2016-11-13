865cae95dec447917bd5ed70ebd9a6fb9463a5b5
Vinicius Almeida Alves <vinicius.alves@poli.ufrj.br>
2016-11-13 18:29:21 -0200

    Update settings.py

--- agendae_backend/settings.py
+++ agendae_backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
+# -*- coding: utf-8 -*-
 
 import os
 
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
