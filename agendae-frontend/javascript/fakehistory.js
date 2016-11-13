d36f2e76a7cf7c38fb13f1e2985a9d012896c28f
Caio Ramos <caiotecn@yahoo.com.br>
2016-11-13 19:03:44 -0200

    a

---
+++ agendae-frontend/javascript/fakehistory.js
@@ -0,0 +1,32 @@
+fakehistory = [
+  {
+    id_schedule: 38,
+    name: "res1",
+    username : "GHT",
+    date: "10/10/2016",
+    return: "12/10/2016",
+  },
+  {
+    id_schedule: 45,
+    name: "res1",
+    username : "Fábio",
+    date: "12/10/2016",
+    return: "13/10/2016",
+  },
+  {
+    id_schedule: 55,
+    name: "res1",
+    username : "Hilmer",
+    date: "13/10/2016",
+    return: "15/10/2016",
+  },
+  {
+    id_schedule: 80,
+    name: "res1",
+    username : "Talita",
+    date: "15/10/2016",
+    return: "pendente",
+  }
+
+];
+
